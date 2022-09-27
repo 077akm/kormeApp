b9497c552fcca71c811b76dbe672ac56f2cd72b5
Yesko <yeskendir.akimov@narxoz.kz>
2022-09-27 16:13:57 +0300

    First commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Create Posts</title>
+</head>
+<body>
+<a href="{{route('posts.index')}}">Go To Index Page</a><br>
+    <form action="{{route('posts.store')}}" method="post">
+        @csrf
+        Title: <input type="text" name="title"> <br><br>
+        Content: <textarea name="content" cols="30" rows="10"></textarea><br><br>
+        <button type="submit">Save Post</button>
+    </form>
+</body>
+</html>
